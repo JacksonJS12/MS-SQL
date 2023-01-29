526ec48d2eddb039b56abebb464b1d8d2606a907
Deniz <d.ivan0v.6100@gmail.com>
2023-01-29 23:59:15 +0200

    COMMIT

--- 02.Exercise-Databases-Introduction/SQLQuery_1.sql
+++ 02.Exercise-Databases-Introduction/SQLQuery_1.sql
@@ -13,9 +13,21 @@ CREATE TABLE [Towns](
     [Name] VARCHAR(100)
 )
 
+ALTER TABLE [Minions]
+    ADD [TownId] INT FOREIGN KEY REFERENCES Towns(Id)
+    
+ALTER TABLE Orders
+ADD FOREIGN KEY (PersonID) REFERENCES Persons(PersonID);
+
 ALTER TABLE Minions
 ALTER COLUMN Id INT NOT NULL;
 
-
 ALTER TABLE Minions
 ADD CONSTRAINT PK_Id  PRIMARY KEY (Id);
+
+INSERT INTO Minions
+    (Id, [Name], Age, TownId)
+VALUES
+(1, 'Kevin',22, 1)
+(2, 'Bob',3, 3)
+(3, 'Steward', NULL, 2)
