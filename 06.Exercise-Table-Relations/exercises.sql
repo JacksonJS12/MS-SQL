9ac187d6176853b27fd74d64e3566a268a5a349e
Deniz Ivanov <d.ivan0v6100@gmail.com>
2023-02-03 16:29:20 +0200

    04 and 06 are done

--- 06.Exercise-Table-Relations/exercises.sql
+++ 06.Exercise-Table-Relations/exercises.sql
@@ -32,31 +32,141 @@ INSERT INTO [Persons]([FirstName], [Salary], [PassportID])
 ('Yana', 60200.00, 101)
 
  -- Problem 02
- CREATE TABLE [Manufactures]
+ CREATE TABLE Manufacturers
  (
-     [ManufacturerID] INT PRIMARY KEY IDENTITY,
-     [Name] VARCHAR(50) NOT NULL,
-     [EstablishedOn] DATETIME2 NOT NULL
+ 	ManufacturerID INT PRIMARY KEY IDENTITY,
+ 	[Name] NVARCHAR(10) NOT NULL,
+ 	EstablishedOn DATE
  )
-
- CREATE TABLE [Models]
+ CREATE TABLE Models
  (
-     [ModelID] INT PRIMARY KEY IDENTITY(101,1),
-     [Name] VARCHAR(50) NOT NULL,
-     [ManufacturerID] INT FOREIGN KEY REFERENCES[Manufactures]([ManufacturerID]) NOT NULL
+ 	ModelID INT PRIMARY KEY IDENTITY(101, 1),
+ 	[Name] NVARCHAR(50) NOT NULL,
+ 	ManufacturerID INT FOREIGN KEY REFERENCES Manufacturers(ManufacturerID)
  )
-
- INSERT INTO [Manufactures]([Name], [EstablishedOn])
-        VALUES      
-                ('BMW', '07/03/1916'),
-                ('Tesla', '01/01/2003'),
-                ('Lada', '01/05/1966')
- INSERT INTO [Models]([Name], [ManufacturerID])
-        VALUES 
-                ('X1', 1),
-                ('i6', 1),
-                ('Model S', 2),
-                ('Model X', 2),
-                ('Model 3', 2),
-                ('Nova', 3)
  
+ INSERT INTO Manufacturers([Name])
+ 	 VALUES
+ ('BMW'),
+ ('Tesla'),
+ ('Lada')
+ 
+ 
+ INSERT INTO Models([Name], ManufacturerID)
+ 	 VALUES
+ ('X1', 1),
+ ('i6', 1),
+ ('ModelS', 2),
+ ('ModelX', 2),
+ ('Model3', 2),
+ ('Nova', 3)
+
+ --Problem 03
+ 
+CREATE TABLE Students
+(
+	StudentID INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(80) NOT NULL
+)
+
+CREATE TABLE Exams
+(
+	ExamID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] NVARCHAR(80) NOT NULL
+)
+
+CREATE TABLE StudentsExams
+(
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+	ExamID INT FOREIGN KEY REFERENCES Exams(ExamID)
+	PRIMARY KEY (StudentID, ExamID)
+)
+
+INSERT INTO Students ([Name]) VALUES
+('Mila'),
+('Tony'),
+('Ron')
+
+INSERT INTO Exams ([Name]) VALUES
+('SpringMVC'),
+('Neo4j'),
+('Oracle 11g')
+
+INSERT INTO StudentsExams(StudentID, ExamID) VALUES
+(1, 101),
+(1, 102),
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103)
+
+--Problem 04
+CREATE TABLE [Teachers]
+(
+    [TeacherID] INT PRIMARY KEY IDENTITY(101,1),
+    [Name] NVARCHAR(50) NOT NULL,
+    [ManagerID] INT FOREIGN KEY REFERENCES[Teachers]([TeacherID])
+)
+
+INSERT INTO Students ([Name]) VALUES
+('Mila'),
+('Tony'),
+('Ron')
+
+INSERT INTO Exams ([Name]) VALUES
+('SpringMVC'),
+('Neo4j'),
+('Oracle 11g')
+
+INSERT INTO [Teachers]([Name], [ManagerID])
+    VALUES
+            ('John', NULL),
+            ('Maya', 106),
+            ('Silvia', 196),
+            ('Ted', 105),
+            ('Mark', 101),
+            ('Greta', 101)
+
+--Problem 06
+CREATE DATABASE [UniversityDatabase2023]
+
+GO
+
+USE [UniversityDatabase2023]
+
+GO
+
+CREATE TABLE [Majors]
+(
+    [MajorID] INT PRIMARY KEY IDENTITY,
+    [Name] NVARCHAR(50) NOT NULL 
+)        
+
+CREATE TABLE [Subjects]
+(
+    [SubjectID] INT PRIMARY KEY IDENTITY,
+    [SubjectName] NVARCHAR(100) NOT NULL
+)
+
+CREATE TABLE [Students] 
+(
+    [StudentID] INT PRIMARY KEY IDENTITY,
+    [StudentNumber] VARCHAR,
+    [StudentName] NVARCHAR(50) NOT NULL,
+    [MajorID] INT FOREIGN KEY REFERENCES[Majors]([MajorID]) NOT NULL 
+)
+
+CREATE TABLE [Agenda]
+(
+    [StudentID] INT FOREIGN KEY REFERENCES[Students]([StudentID]),
+    [SubjectID] INT FOREIGN KEY REFERENCES[Subjects]([SubjectID]),
+    PRIMARY KEY([StudentID], [SubjectID])
+)
+
+CREATE TABLE [Payments]
+(
+    [PaymentID] INT PRIMARY KEY IDENTITY,
+    [PaymentDate] DATETIME2 NOT NULL,
+    [PaymentAmount] DECIMAL(8, 2) NOT NULL,
+    [StudentID] INT FOREIGN KEY REFERENCES [Students]([StudentID]) NOT NULL
+ )
