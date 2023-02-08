c79efebc5afc57720d09daad7758143b6d631dae
Deniz Ivanov <d.ivan0v6100@gmail.com>
2023-02-08 15:56:32 +0200

    ex 17 is done and init

--- 08.Exercise-Built-in-Functions/exercises.sql
+++ 08.Exercise-Built-in-Functions/exercises.sql
@@ -157,14 +157,14 @@ ORDER BY[Email Provider],
   SELECT [Name]
       AS [Game],
          CASE
-            WHEN DATEPART(HOUR, [Start]) > 0 AND DATEPART(HOUR, [Start]) < 12 THEN 'Morning'
+            WHEN DATEPART(HOUR, [Start]) >= 0 AND DATEPART(HOUR, [Start]) < 12 THEN 'Morning'
             WHEN DATEPART(HOUR, [Start]) >= 12 AND DATEPART(HOUR, [Start]) < 18 THEN 'Afternoon'
             ELSE  'Evening'
          END
       AS [Part of the Day],
          CASE
             WHEN [Duration] <= 3 THEN 'Extra Short'
-            WHEN [Duration] BETWEEN 4 AND 6 THEN 'Short'
+            WHEN [Duration] >= 4 AND [Duration] <= 6 THEN 'Short'
             WHEN [Duration] > 6 THEN 'Long'
             ELSE 'Extra Long'
          END
