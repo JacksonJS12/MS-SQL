fe9c201a23812375cf9af18b32ea8f6da9830b0c
Deniz <d.ivan0v.6100@gmail.com>
2023-02-09 07:50:04 +0200

    commit

--- 10.Exercise-Subqueries-and-Joins/exercises.sql
+++ 10.Exercise-Subqueries-and-Joins/exercises.sql
@@ -16,3 +16,48 @@ LEFT JOIN [Addresses]
      AS [a]
      ON [e].[AddressID] = [a].[AddressID]
 ORDER BY[e].[AddressID] 
+
+-- Problem 05
+   SELECT 
+  TOP (3) [e].[EmployeeID],
+          [e].[FirstName]
+     FROM [Employees]
+       AS [e]
+LEFT JOIN [EmployeesProjects]
+       AS [ep]
+       ON [e].[EmployeeID] = [ep].[EmployeeID]
+    WHERE [ep].[ProjectID] IS NULL
+ ORDER BY [e].[EmployeeID]
+
+
+ -- Problem 07
+     SELECT 
+     TOP (5) [e].[EmployeeID],
+               [e].[FirstName],
+               [p].[Name]
+          AS [ProjectName]
+          FROM [EmployeesProjects]
+          AS [ep]
+     INNER JOIN [Employees]
+          AS [e]
+          ON [ep].[EmployeeID] = [e].[EmployeeID]
+     INNER JOIN [Projects]
+          AS [p]
+          ON [ep].[ProjectID] = [p].[ProjectID]
+          WHERE [p].[StartDate] > '08-13-2002' AND [p].[EndDate] IS NULL
+     ORDER BY [e].[EmployeeID]
+
+
+-- Problem 09
+   SELECT [e].[EmployeeID],
+          [e].[FirstName],
+          [e].[ManagerID],
+          [m].[FirstName]
+       AS [ManagerName]
+      FROM [Employees]
+        AS [e]
+INNER JOIN [Employees]
+        AS [m]
+        ON [e].[EmployeeID] = [m].[EmployeeID]
+     WHERE [e].[ManagerID] IN (3, 7)
+  ORDER BY [m].[EmployeeID]
