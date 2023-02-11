7cce878d1c2213644995d52b45f4dfe929f94720
Deniz <d.ivan0v.6100@gmail.com>
2023-02-11 19:58:18 +0200

    added some exercises

--- 10.Exercise-Subqueries-and-Joins/exercises.sql
+++ 10.Exercise-Subqueries-and-Joins/exercises.sql
@@ -17,6 +17,22 @@ LEFT JOIN [Addresses]
      ON [e].[AddressID] = [a].[AddressID]
 ORDER BY[e].[AddressID] 
 
+
+-- Problem 04
+  SELECT 
+  TOP(5) [EmployeeID], 
+         [FirstName], 
+         [Salary], 
+         [d].[Name] 
+      AS [DepartmentName]
+	  FROM [Employees] 
+      AS [e]
+    JOIN [Departments] 
+      AS [d]
+		 ON [d].[DepartmentID] = [e].[DepartmentID]
+   WHERE [e].[Salary] > 15000
+ORDER BY [d].[DepartmentID] ASC
+
 -- Problem 05
    SELECT 
   TOP (3) [e].[EmployeeID],
@@ -28,6 +44,22 @@ LEFT JOIN [EmployeesProjects]
        ON [e].[EmployeeID] = [ep].[EmployeeID]
     WHERE [ep].[ProjectID] IS NULL
  ORDER BY [e].[EmployeeID]
+
+
+-- Problem 06
+   SELECT [e].[FirstName], 
+          [e].[LastName], 
+          [e].[HireDate],
+          [d].[Name] 
+       AS [DeptName]
+     FROM [Employees]
+       AS [e]
+     JOIN [Departments]
+       AS [d] 
+   	 ON [d].[DepartmentID] = [e].[DepartmentID]
+    WHERE [e].[HireDate] > '1999-01-01'
+   	AND [d].[Name] IN ('Sales', 'Finance')
+ ORDER BY [e].[HireDate] ASC
 
 
  -- Problem 07
@@ -49,18 +81,17 @@ LEFT JOIN [EmployeesProjects]
 
 
 -- Problem 09
-   SELECT [e].[EmployeeID],
-          [e].[FirstName],
-          [e].[ManagerID],
-          [m].[FirstName]
-       AS [ManagerName]
-      FROM [Employees]
-        AS [e]
-INNER JOIN [Employees]
-        AS [m]
-        ON [e].[EmployeeID] = [m].[EmployeeID]
-     WHERE [e].[ManagerID] IN (3, 7)
-  ORDER BY [m].[EmployeeID]
+SELECT [e].[EmployeeID], 
+		 [e].[FirstName], 
+		 [e].[ManagerID], 
+	   	 [m].[FirstName] 
+      AS [ManagerName]
+    FROM [Employees] 
+      AS [e]
+    JOIN [Employees] AS [m]
+      ON [m].[EmployeeID] = [e].[ManagerID]
+   WHERE [m].[EmployeeID] IN (3, 7)
+ORDER BY [e].[EmployeeID] ASC
 
 
 
